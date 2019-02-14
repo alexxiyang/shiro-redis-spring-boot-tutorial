4eb697718a1939185b7c2c21843d3a82065ce58a
Alex Yang <alex.yang@ehealth.com>
2019-02-14 18:20:58 +0800

    Add logout link

--- src/main/java/org/crazycake/shiroredisspringboottutorial/controller/LoginController.java
+++ src/main/java/org/crazycake/shiroredisspringboottutorial/controller/LoginController.java
@@ -5,9 +5,12 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.crazycake.shiroredisspringboottutorial.model.LoginForm;
-import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
@@ -28,7 +31,14 @@ public class LoginController {
 
         checkAuthorization(currentUser);
 
-        return "<h1>Hello " + loginForm.getUsername() + "</h1><p>session: " + session.getId();
+        return "<h1>Hello " + loginForm.getUsername() + "</h1><p>session: " + session.getId() + "<br><a href='/logout'>Logout</a>";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        Subject currentUser = SecurityUtils.getSubject();
+        currentUser.logout();
+        return "You've logout";
     }
 
     private void checkSession(Session session) {
